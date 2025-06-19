fe40c3a5c18ca1d63ca4be897e94e1f673c2f8f0
RKC-Infratech <imagineeringbridgesindia@gmail.com>
2025-06-19 20:33:36 +0530

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -16,8 +16,8 @@ const Hero = ({ title, subtitle, backgroundImages, children, className = "" }: H
     "/lovable-uploads/award5.jpg",
     "/lovable-uploads/cover2.jpg",
     "/lovable-uploads/cover.jpg",
-    "https://images.unsplash.com/photo-1439337153520-7082a56a81f4?ixlib=rb-4.0.3&auto=format&fit=crop&w=2000&q=80",
-    "https://images.unsplash.com/photo-1554224155-6726b3ff858f?ixlib=rb-4.0.3&auto=format&fit=crop&w=2000&q=80"
+    "/lovable-uploads/award.jpg",
+    "/lovable-uploads/award1.jpg"
   ];
   
   const images = backgroundImages || defaultImages;
